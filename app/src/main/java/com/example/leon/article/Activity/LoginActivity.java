2053a654b622ae5d985127a0295a9284b357b151
Administrator <1103946342@qq.com>
2017-05-10 08:56:16 +0800

    Initial commit leon

---
+++ app/src/main/java/com/example/leon/article/Activity/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.leon.article.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.leon.article.R;
+
+/**
+ * loginActivity
+ * Created by leonseven on 2017/5/9.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
